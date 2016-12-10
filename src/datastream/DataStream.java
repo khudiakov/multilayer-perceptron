5938b87e4a100c053083b1be180ad5da53746732
Khudiakov Daniil <khudiakov@outlook.com>
2016-12-10 11:23:09 +0100

    added randomization of input data

--- src/datastream/DataStream.java
+++ src/datastream/DataStream.java
@@ -3,36 +3,57 @@ package datastream;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.*;
 
 /**
  * Created by khudiakov on 24/10/2016
  */
 public  class DataStream {
     final private BufferedReader fileStream;
+    final private int batchLimit = 100;
+    private boolean randomize;
+    private int nInput;
+    private int nOutput;
+    private List<Data> batch = new ArrayList<>();
 
-    public DataStream(String filepath) throws IOException {
+    public DataStream(String filepath, int nInput, int nOutput, boolean randomize) throws IOException {
         fileStream = new BufferedReader(new FileReader(filepath));
-        // skip description line
-        fileStream.readLine();
+        this.randomize = randomize;
+        this.nInput = nInput;
+        this.nOutput = nOutput;
     }
 
-    public Data next() throws IOException {
-        String line = fileStream.readLine();
-        if (line == null) {
-            return null;
+    private void loadBatch() throws IOException {
+        int count = 0;
+        batch.clear();
+        while (count < batchLimit) {
+            count++;
+            String line = fileStream.readLine();
+            if (line == null) {
+                return;
+            }
+
+            String[] values = line.split(",");
+            double[] inputs = new double[this.nInput];
+            for (int i=0; i<inputs.length; i++) {
+                inputs[i] = Double.parseDouble(values[i]);
+            }
+
+            double[] outputs = new double[this.nOutput];
+            for (int i=0; i<outputs.length; i++) {
+                outputs[i] = Double.parseDouble(values[nInput+i]);
+            }
+            batch.add(new Data(inputs, outputs));
         }
+    }
 
-        String[] values = line.split(",");
-
-        double[] inputs = new double[values.length-1];
-        for (int i=0; i<inputs.length; i++) {
-            inputs[i] = Double.parseDouble(values[i]);
+    public List<Data> getNextBatch() throws IOException {
+        this.loadBatch();
+        if (this.randomize) {
+            long seed = System.nanoTime();
+            Collections.shuffle(batch, new Random(seed));
         }
-
-        double[] outputs = new double[1];
-        outputs[0] = Double.parseDouble(values[values.length-1]);
-
-        return new Data(inputs, outputs);
+        return this.batch;
     }
 
     protected void finalize() throws Throwable {
